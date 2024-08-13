2dd0465000e7c65870370cf5f58c4b7a3aa63cb2
rapanhalucky <rapanhalucky@gmail.com>
2024-08-13 18:23:43 +0700

    update

---
+++ resources/js/Components/ItemProducts.vue
@@ -0,0 +1,19 @@
+<script setup>
+// DefineProps
+import { defineProps } from "vue";
+const props = defineProps({
+    DataLoops: Array, // Data type Array
+});
+
+console.log(props.DataLoops);
+</script>
+
+<template>
+    <div v-for="(item, index) in DataLoops" :key="index">
+        <p>Product name: {{ item.name }}</p>
+        <p>Branch {{ item.branch }}</p>
+        <p>Price: {{ item.price }}</p>
+    </div>
+</template>
+
+<style></style>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,6 +1,30 @@
 <script setup>
 import Header from "../Components/Header.vue";
 import Body from "../Components/Body.vue";
+
+// import components
+import ItemProducts from "../Components/ItemProducts.vue";
+
+const dataProducts = [
+    {
+        id: 1,
+        name: "sndgfan",
+        branch: "Nike", // Object one
+        price: 10,
+    },
+    {
+        id: 2,
+        name: "fasjdgbi",
+        branch: "Nike", // Object two
+        price: 10,
+    },
+    {
+        id: 2,
+        name: "fasjdgbi",
+        branch: "Nike", // Object two
+        price: 10,
+    },
+];
 </script>
 
 <template>
@@ -8,6 +32,8 @@ import Body from "../Components/Body.vue";
         <h3>This Home page</h3>
         <Header />
         <Body />
+        <ItemProducts :DataLoops="dataProducts" />
+        <ItemProducts :DataLoops="dataProducts" />
     </div>
 </template>
 
